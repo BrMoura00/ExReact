c6b51335c72e017b036f8869b7131216aacc7944
Vinícius Danciguer Marafiga <marafigabusines@gmail.com>
2024-03-27 02:59:22 -0300

    troca de posição dos bagulho

--- reactAbasEx/src/App.jsx
+++ reactAbasEx/src/App.jsx
@@ -3,35 +3,51 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+function App() {  
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className='title-container'>
-        <h1>Vite</h1>
-        <h1>React</h1>
-        <h1>Axios</h1>
-        <h1>React Router DOM</h1>
-        <h1>Npm</h1>
-        <h1>NODE.js</h1>
-        
-      </div>
-      
-      <div className='logo-container'>
-        
-        <img src={viteLogo} className="logo" alt="Vite logo" />
-      
-        <img src={reactLogo} className="logo react" alt="React logo" />
-      
-        <img src="../public/imgs/axios_logo.png"  className="logo-axios"alt="axiosLogo"/>
-      
-        <img src="../public/imgs/react-router.png" className="logo-router" alt="routerLogo"/>
-      
-        <img src="../public/imgs/npm_logo.png" className='logo-npm' alt="npmLogo" />
+      <div className='container-father'>
 
-        <img src="../public/imgs/node_logopng.png" className='logo-node' alt="nodeLogo" />
+        <div className='container-one'>
+          <h1>Vite</h1>
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <p>Vite Legal</p>
+        </div>
+      
+        <div className='container-two'>
+          <h1>React</h1>
+          <img src={reactLogo} className="logo" alt="React logo" />
+          <p>React Legal</p>
+        </div>
+
+        <div className='container-tree'>
+          <h1>Axios</h1>
+          <img src="../public/imgs/axios_logo.png" className='logo-axios' alt="Axios logo" />
+          <p>Axios top de linha</p>
+        </div>
+
+        <div className='container-four'>
+          <h1> REACT ROUTER</h1>
+          <img src="../public/imgs/react-router.png" className='logo-router' alt="Router logo" />
+          <p>REACT ROUTER top</p>
+        </div>
+
+        <div className='container-five'>
+          <h1>Npm</h1>
+          <img src="../public/imgs/npm_logo.png" className='logo-npm' alt="Npm logo" />
+          <p>Npm top de linha</p>
+        </div>
+
+        <div className='container-six'>
+          <h1>Node</h1>
+          <img src="../public/imgs/node_logopng.png"  className='logo-node' alt="Node logo" />
+          <p>Node daora meu</p>
+        </div>
+
       </div>
+      
       
     </>
   );
